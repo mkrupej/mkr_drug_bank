cce63febd16c7f6b1db2bc260917c04856defe7d
michal <michalkrupej@gmail.com>
2018-02-01 23:19:35 +0100

    drug presentating added

--- Model/Drug.py
+++ Model/Drug.py
@@ -1,3 +1,4 @@
+import collections
 import xml.sax
 import Parser
 
@@ -18,12 +19,42 @@ class Drug:
 
     def __repr__(self):
 
-        detailed_represent = ''
+        detailed_represent = 'IDENTIFICATION\n'+self.general_represent
 
         for key, value in self.elem_dict["drug"].items():
-            if key in ["name", "drugbank-id", "description"]:
-                self.general_represent += "{}: {}\n".format(key.upper(), value)
-                #print("{}: {}".format(key.upper(), value))
-        #for key, value in self.elem_dict["drug"].items():
-            #print("{} wartosc {}".format(key.upper(), value))
+            if key not in ["name", "drugbank-id", "description"]:
+                detailed_represent += "\n{} : ".format(key.upper())
+                if type(value) is collections.OrderedDict:
+                    detailed_represent += Drug.extract_from_dict(value)
+                else:
+                    detailed_represent += "{}\n".format(value)
+        return detailed_represent.replace("\n\n", "\n")
+
+    @staticmethod
+    def extract_from_list(property_list):
+        detailed_represent = "\t"
+        for elem in property_list:
+            if type(elem) is collections.OrderedDict:
+                detailed_represent += Drug.extract_from_dict(elem)
+            elif type(elem) is list:
+                detailed_represent += elem
         return detailed_represent
+
+    @staticmethod
+    def extract_from_dict(property_dict):
+        detailed_represent = "\n\t"
+
+        for elem in property_dict:
+           # print(type(property_dict[elem]), property_dict[elem])
+            detailed_represent += "{} : ".format(elem.upper())
+            if type(property_dict[elem]) is collections.OrderedDict:
+                detailed_represent += Drug.extract_from_dict(property_dict[elem])
+            elif type(property_dict[elem]) is list:
+                detailed_represent += Drug.extract_from_list(property_dict[elem])
+            else:
+                detailed_represent += "{}\n\t".format(property_dict[elem])
+
+        return detailed_represent
+
+
+
--- main.py
+++ main.py
@@ -1,19 +1,22 @@
+import collections
+
 import Parser.Drug
 import Loader.Loader
 import Model.Drug
 
 #filename = './Source/minidrug2.xml'
 filename = './Source/full database.xml'
+#filename = './Source/single_drug.xml'
+
 path = 'drug'
 incremental_loader = Loader.Loader.load(filename, path)
-search_dict = {"name": 'Lepirudin'}
+search_dict = {"name": 'Patritumab'}
 
-
-#search2(incremental_loader, search_dict)
 
 a = Parser.Drug.Drug(incremental_loader, search_dict)
-#print(type(a.result_dicts_list[0]))
-#print(a.result_dicts_list)
 
-drug = Model.Drug.Drug(a.result_dicts_list[0])
-print(drug.general_represent)
+print(a.result_dicts_list)
+for elem in a.result_dicts_list:
+    drug = Model.Drug.Drug(elem)
+    print(drug.general_represent)
+   # print(drug)
