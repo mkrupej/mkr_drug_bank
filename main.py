c92b31b81a5b8defa93f74a42ca8ad473801fdbe
michal <michalkrupej@gmail.com>
2018-02-07 19:00:47 +0100

    tests updated DB path updated

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import Loader.Loader
 import Model.Drug
 
 
-db_file = 'C:/Users/michal/PycharmProjects/mkr_drug_bank/Source/full database.xml' #set absoulte path to DB file
+db_file = 'C:/full database.xml' #set absoulte path to DB file
 
 drug_xml_selector = 'drug'
 incremental_loader = Loader.Loader.load(db_file, drug_xml_selector) #set loader
 
-search_dict = {"name": 'Action Pathway', "category" : "drug_action"} #set search criteria {"property_name": "value"}
-
+#SIMPLE
+# search_dict = {"name": 'Lepirudin'} #set search criteria {"property_name": "value"}
+#
 #
 # a = Parser.Search.Search(incremental_loader, search_dict)
 #
@@ -18,6 +19,9 @@ search_dict = {"name": 'Action Pathway', "category" : "drug_action"} #set search
 #     drug = Model.Drug.Drug(elem)
 #     print(drug.general_represent)
 #     print(drug)
+
+#ADVANCED
+#search_dict = {"name": 'Action Pathway', "category" : "drug_action"} #set search criteria {"property_name": "value"}
 
 # b = Parser.AdvancedSearch.AdvancedSearch(incremental_loader, search_dict, search_type=Parser.AdvancedSearch.SupportedFile.PATHWAY)
 # #
